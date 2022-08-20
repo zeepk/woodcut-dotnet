b9df499c2d840fe0c484a44ba9f49fb96b5330df
Matt Hughes <mhughes.ipod@gmail.com>
2022-08-19 22:09:58 -0700

    woodcut spindown

--- frontend/src/features/Common/LandingPage.tsx
+++ frontend/src/features/Common/LandingPage.tsx
@@ -1,29 +1,13 @@
-import React from 'react';
-import { useAppSelector } from '../../app/hooks';
+import React from "react";
+import { useAppSelector } from "../../app/hooks";
 
-import { selectAuthIsLoggedIn, selectAuthLoading } from '../Common/commonSlice';
-import NewUserLandingPage from 'features/RS3/Dashboard/NewUserLandingPage';
-import OsrsNewUserLandingPage from 'features/OSRS/Dashboard/OsrsNewUserLandingPage';
-import ExistingUserLandingPage from 'features/RS3/Dashboard/ExistingUserLandingPage';
-import LoadingIcon from 'features/Common/LoadingIcon';
-import 'features/Common/common.scss';
+import { selectAuthIsLoggedIn, selectAuthLoading } from "../Common/commonSlice";
+import NewUserLandingPage from "features/RS3/Dashboard/NewUserLandingPage";
+import OsrsNewUserLandingPage from "features/OSRS/Dashboard/OsrsNewUserLandingPage";
+import ExistingUserLandingPage from "features/RS3/Dashboard/ExistingUserLandingPage";
+import LoadingIcon from "features/Common/LoadingIcon";
+import "features/Common/common.scss";
 
 export function LandingPage() {
-	const isLoggedIn = useAppSelector(selectAuthIsLoggedIn);
-	const loading = useAppSelector(selectAuthLoading);
-	const isOsrs = window.location.href.includes('osrs');
-
-	if (loading) {
-		return <LoadingIcon fullScreen={true} />;
-	}
-
-	if (isLoggedIn) {
-		return <ExistingUserLandingPage />;
-	}
-
-	if (isOsrs) {
-		return <OsrsNewUserLandingPage />;
-	}
-
-	return <NewUserLandingPage />;
+  return <NewUserLandingPage />;
 }
--- frontend/src/features/RS3/Dashboard/NewUserLandingPage.tsx
+++ frontend/src/features/RS3/Dashboard/NewUserLandingPage.tsx
@@ -1,64 +1,48 @@
-import React, { useEffect } from 'react';
-import { useAppSelector, useAppDispatch } from 'app/hooks';
+import React, { useEffect } from "react";
+import { useAppSelector, useAppDispatch } from "app/hooks";
 
-import { ProgressBar } from 'primereact/progressbar';
+import { ProgressBar } from "primereact/progressbar";
 
 import {
-	selectActivities,
-	selectActivitiesLoading,
-	getRs3Activities,
-} from 'features/RS3/rs3Slice';
-import { ActivityList } from 'features/RS3/Dashboard/ActivityList';
-import { LoginButton } from 'features/Common/Account/LoginButton';
-import { CreateAccountButton } from 'features/Common/Account/CreateAccountButton';
-import '../rs3.scss';
+  selectActivities,
+  selectActivitiesLoading,
+  getRs3Activities,
+} from "features/RS3/rs3Slice";
+import { ActivityList } from "features/RS3/Dashboard/ActivityList";
+import { LoginButton } from "features/Common/Account/LoginButton";
+import { CreateAccountButton } from "features/Common/Account/CreateAccountButton";
+import "../rs3.scss";
 import {
-	homeContentTextPre,
-	homeContentTextPost,
-	activityFeedTitleText,
-	rs3HomePageActivities,
-	twitterUsername,
-	twitterUrl,
-} from 'utils/constants';
-import Logo from 'assets/images/logo.png';
+  homeContentTextPre,
+  homeContentTextPost,
+  activityFeedTitleText,
+  rs3HomePageActivities,
+  twitterUsername,
+  twitterUrl,
+} from "utils/constants";
+import Logo from "assets/images/logo.png";
 
 export default function NewUserLandingPage() {
-	const dispatch = useAppDispatch();
-
-	const activities = useAppSelector(selectActivities);
-	const loading = useAppSelector(selectActivitiesLoading);
-
-	useEffect(() => {
-		dispatch(getRs3Activities(rs3HomePageActivities));
-	}, [dispatch]);
-
-	return (
-		<div className="container--logged-out-user p-d-flex p-jc-between p-p-5 p-flex-wrap p-flex-md-nowrap">
-			<div className="container--half p-pl-1 p-pl-lg-5">
-				<img className="img--home-logo" src={Logo} alt="logo" />
-				<div className="container--home-text p-my-2">
-					<span className="text--home-content">{homeContentTextPre}</span>
-					<a className="text--home-content twitter p-mx-1" href={twitterUrl}>
-						{twitterUsername}
-					</a>
-					<span className="text--home-content">{homeContentTextPost}</span>
-				</div>
-				<div className="p-d-flex container--home-buttons p-mt-4">
-					<LoginButton />
-					<CreateAccountButton />
-				</div>
-			</div>
-			<div className="container--half p-d-flex p-flex-column p-ai-center">
-				<h2>{activityFeedTitleText}</h2>
-				{loading ? (
-					<ProgressBar
-						className="progressbar--activity-list p-mt-6"
-						mode="indeterminate"
-					/>
-				) : (
-					<ActivityList activities={activities} />
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className="container--logged-out-user p-d-flex p-jc-between p-p-5 p-flex-wrap p-flex-md-nowrap">
+      <div className="container--half p-pl-1 p-pl-lg-5">
+        <img className="img--home-logo" src={Logo} alt="logo" />
+        <div className="container--home-text p-my-2">
+          <div className="text--home-content">Thanks for using Woodcut!</div>
+          <br />
+          <span className="text--home-content">
+            {`Things weren't going well so I'm rewriting the site from scratch. If
+            you're interested in the process or have suggestions, stop by my `}
+          </span>
+          <a
+            className="text--home-content twitter p-mx-1"
+            href={"https://www.twitch.tv/zee_pk"}
+          >
+            twitch channel
+          </a>
+          <span className="text--home-content">{" and say hi!"}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
